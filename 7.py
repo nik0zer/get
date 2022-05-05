08b503dff2567f92f9ffb85228f298de570ebb15
nik0zer <s21b1.nikozer@gmail.com>
2022-05-05 12:49:39 +0300

    asf

--- 7.py
+++ 7.py
@@ -56,30 +56,42 @@ try:
     vals_of_capasitor = []
     IO.output(troyka, IO.HIGH)
     time_of_start = tm.time()
+    print("start of capasitor charge")
+    #start of capasitor charge
     while(adc_new() <= 256 * 0.97):
         val = adc_new()
         IO.output(leds, decimal2binary(val))
         vals_of_capasitor.append(val)
         tm.sleep(0.001)
-        print(val)
+        print(str(val) + ": points of ADC")
+    print("start of capasitor discharge")
+    #start of capasitor discharge
     IO.output(troyka, IO.LOW)
     while(adc_new() >= 256 * 0.02):
         val = adc_new()
         IO.output(leds, decimal2binary(val))
         vals_of_capasitor.append(val)
         tm.sleep(0.001)
-        print(val)
+        print(str(val) + ": points of ADC")
+    #end of capasitor discharge
+    print("end of capasitor discharge")
     time_of_end = tm.time()
     plt.plot(vals_of_capasitor)
+    print("load in files")
+    #load in files
     with  open("data.txt", 'w')  as  data:
         for i in range(len(vals_of_capasitor)):
              data.write(str(vals_of_capasitor[i]) + '\n')
     with  open("settings.txt", 'w')  as  settings:
-        settings.write(str(time_of_end - time_of_start) +'\n')
-        settings.write(str((time_of_end - time_of_start)/ len(vals_of_capasitor))+'\n')
-        settings.write(str(len(vals_of_capasitor) / (time_of_end - time_of_start))+'\n')
-        settings.write(str(3.3*0.02)+"-"+str(3.3*0.97)+'\n')
+        settings.write("Время эксперимента = "+ str((time_of_end - time_of_start) / len(vals_of_capasitor))+ '\n')
+        settings.write("Время периода = " + str( time_of_end - time_of_start) + '\n')
+        settings.write("Средняя частота дискретизации = " + str(len(vals_of_capasitor) / (time_of_end - time_of_start))+ '\n')
         settings.write(str(3.3/256)+'\n')
+
+    print("Время эксперимента = "+ str((time_of_end - time_of_start) )+ '\n')
+    print("Время периода = " + str(( time_of_end - time_of_start)/ len(vals_of_capasitor)) + '\n')
+    print("Средняя частота дискретизации = " + str(len(vals_of_capasitor) / (time_of_end - time_of_start))+ '\n')
+    print("Шаг по напряжению = " + str(3.3/256)+'\n')
     plt.show()
 finally:
     IO.output(dac, 0)
